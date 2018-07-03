f428a1f8f63ebb7b802d83ef9fafd4816ba2d4a9
JunoJulius <yunoyulius@gmail.com>
2018-07-03 14:53:52 +0200

    2nd commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import 'reflect-metadata'
-import {createKoaServer} from "routing-controllers"
-import Controller from "./controller"
-
-const port = process.env.PORT || 4000
+import {createKoaServer} from 'routing-controllers'
+import PageController from './pages/controller'
+import setupDb from './db'
 
 const app = createKoaServer({
-   controllers: [Controller]
+  controllers: [
+    PageController
+  ]
 })
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+setupDb()
+  .then(_ =>
+    app.listen(4000, () => console.log('Listening on port 4000'))
+  )
+  .catch(err => console.error(err))
+
+
+//   docker run -p 5432:5432 -e POSTGRES_PASSWORD=secret --name=postgres postgres
+// docker run -it --rm --link postgres:postgres postgres psql -h postgres -U postgres
